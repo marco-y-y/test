6adb3511098962280a60812c5a3f44415c606c7c
Marco Yamba <marcoyamba@MacBook-Pro-de-Marco.local>
2022-03-29 21:13:32 -0500

    integración de Unity
    Se a puesto los archivos de Unity para la creación de objetos dinámicos.

---
+++ dynamic-object-creation/Assets/Scripts/origin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class origin : MonoBehaviour
+{
+    public GameObject stone; 
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(stone, transform.position, Random.rotation); 
+        
+        }
+        
+    }
+}
